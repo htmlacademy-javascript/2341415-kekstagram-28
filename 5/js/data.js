a3b9c290766b8fa636969c08ed7e1974ca98c928
Keks <keks@htmlacademy.ru>
2023-04-06 01:37:20 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/data.js
@@ -0,0 +1,59 @@
+import { getRandomArrayElement, getRandomInteger,createRandomUnicInteger } from './util.js';
+
+
+const DESCRIPTIONS = [
+  'Бывало и лучше!',
+  'Эх, как давно это было...',
+  'Замечательное фото!Сразу видно руку профи!',
+  'Это я дома на любимом диване',
+  'Мой кот Зимбабве!'
+];
+
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const NAMES = [
+  'Сильвия',
+  'Лесси',
+  'Катерина',
+  'Елисей',
+  'Андреас',
+  'Иван',
+  'Семен',
+  'Петрович'
+];
+
+const createOrderIdGenerator = (start = 0) => {
+  let count = start;
+  return () => ++count;
+};
+
+const getUrl = createRandomUnicInteger(1, 25);
+const getLikes = createRandomUnicInteger(15, 200);
+const getIdPhoto = createOrderIdGenerator();
+
+const createComment = () => ({
+  id: getRandomInteger(1, 1000),
+  avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+  message: getRandomArrayElement(MESSAGES),
+  name: getRandomArrayElement(NAMES),
+});
+
+
+const createPhotoDescription = () => ({
+  id: getIdPhoto(),
+  url: `photos/${getUrl()}.jpg`,
+  description: getRandomArrayElement(DESCRIPTIONS),
+  likes: getLikes(),
+  comments:Array.from({length: getRandomInteger(1, 3)}, createComment),
+});
+
+const createPhotosDescription = () => Array.from({length: 25}, createPhotoDescription);
+
+export {MESSAGES, NAMES, createPhotosDescription};
---
+++ 5/js/util.js
@@ -0,0 +1,29 @@
+const getRandomInteger = (a, b) => {
+  const min = Math.ceil(Math.min(a, b));
+  const max = Math.floor(Math.max(a, b));
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const getRandomArrayElement = (arr) => arr[getRandomInteger(0, arr.length - 1)];
+
+function createRandomUnicInteger(min, max) {
+  const previousValues = [];
+
+  return () => {
+    if (previousValues.length >= (max - min + 1)) {
+      throw new Error(`Перебраны все числа из диапазона от ${ min } до ${ max }`);
+    }
+
+    let currentValue = getRandomInteger(min, max);
+
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
+    }
+
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+}
+
+export{ getRandomInteger, getRandomArrayElement, createRandomUnicInteger };
